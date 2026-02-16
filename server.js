843c0ada282717f335a196bf8fbd4a6cd625ae81
webalgecom <webalge.com@gmail.com>
2026-02-16 11:41:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.listen(PORT, () => {
   console.log("Servidor funcionando en puerto " + PORT);
 });
